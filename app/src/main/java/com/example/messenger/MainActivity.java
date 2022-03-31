e4a730ebff1efdb3087dbc327d95e71d690ff31f
Lidne <play.gt.play@yandex.ru>
2022-03-31 14:14:44 +0300

    nothing works(

--- app/src/main/java/com/example/messenger/MainActivity.java
+++ app/src/main/java/com/example/messenger/MainActivity.java
@@ -1,10 +1,7 @@
 package com.example.messenger;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
